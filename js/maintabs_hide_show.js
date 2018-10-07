967b217eabf62181e2201eb5c53b7590f239943f
Junyao Yang <ksacreym@gmail.com>
2018-10-07 13:50:54 -0400

    css/js for tab hide/show

---
+++ js/maintabs_hide_show.js
@@ -0,0 +1,15 @@
+/*
+ *hide and show function
+ *alternatively use jquery hide() and show()
+ *last edited 10/07/2018 @Junyao Yang
+ */
+function Open_Tab(tab_name)
+{
+	var i, x, tablinks;
+	x = document.getElementsByClassName("tab_content");
+	for (i=0; i < x.length; i++) {
+		x[i].style.display = "none";
+	}
+
+	document.getElementById(tab_name).style.display = "block";  
+}

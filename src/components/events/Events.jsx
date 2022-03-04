da904a925877da1bcc35dd19d6b1fc60261e005e
Charles Vidro <cjvidro@mtu.edu>
2022-03-04 10:38:38 -0500

    Updated events

--- src/components/events/Events.jsx
+++ src/components/events/Events.jsx
@@ -10,14 +10,14 @@ const UpcomingEvents = () => {
             <h3>Upcoming Events</h3>
           </div>
         </div>
-          <Event title={"First General Body Meeting"} location={"Zoom"} when={"January 12th from 6pm to 7pm."} />
+          <Event title={"Recruitment Meeting"} location={"Rekhi 214 or Zoom"} when={"March 23rd from 6pm to 7pm."} />
           <Event title={"General Body Meetings"} location={"Rekhi 214 or Zoom"} when={"Every Wednesday of the semester from 6pm to 7pm."}>
             <p><strong><a href={"https://michigantech.zoom.us/j/86891464621"} target="_blank" rel="noopener noreferrer">Join Zoom Meeting. <i className="fa fa-external-link" /></a></strong></p>
           </Event>
-          <Event title={"Winter Wonderhack"} location={"Van Pelt and Opie Library"} when={"February 18th through 20th."}>
-            <p>Winter Wonderhack is a 36 hour event where people from all different backgrounds come together, form teams around a problem or idea, and collaboratively plan, design, or code a unique solution from scratch.</p>
-            <p><strong><a href={"https://winterwonderhack.com/#register"} target="_blank" rel="noopener noreferrer">Click here to register! <i className="fa fa-external-link" /></a></strong></p>
-          </Event>
+          {/*<Event title={"Winter Wonderhack"} location={"Van Pelt and Opie Library"} when={"February 18th through 20th."}>*/}
+            {/*<p>Winter Wonderhack is a 36 hour event where people from all different backgrounds come together, form teams around a problem or idea, and collaboratively plan, design, or code a unique solution from scratch.</p>*/}
+            {/*<p><strong><a href={"https://winterwonderhack.com/#register"} target="_blank" rel="noopener noreferrer">Click here to register! <i className="fa fa-external-link" /></a></strong></p>*/}
+          {/*</Event>*/}
       </div>
     </section>
   );

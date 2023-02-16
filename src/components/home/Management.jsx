2b5f85fdb260c0c282c594859ffa22fad73d2842
emmuller-mtu <77806818+emmuller-mtu@users.noreply.github.com>
2023-02-16 12:48:31 -0500

    fixed issues with Management.jsx, added Jeremiah

--- src/components/home/Management.jsx
+++ src/components/home/Management.jsx
@@ -40,23 +40,22 @@ function Management() {
                             <li data-target="#managementCarousel" data-slide-to="13"/>
                             <li data-target="#managementCarousel" data-slide-to="14"/>
                             <li data-target="#managementCarousel" data-slide-to="15"/>
-                            <li data-target="#managementCarousel" data-slide-to="16"/>
-                            //<li data-target="#managementCarousel" data-slide-to="17"/>
+                            {/*Add for each management card*/}
                         </ol>
                     </div>
                     <div className="carousel-inner">
+                        {/*Crop images to a square, convert JPG to jpg if issues*/}
                         <ManagerCard name="Abby Myers" imagePath="/img/abby_m.jpg" role="President" email="abigailm@mtu.edu" active="active"/>
                         <ManagerCard name="Eva Muller" imagePath="/img/eva_m.png" role="Vice President" email="emmuller@mtu.edu" active=""/>
                         <ManagerCard name="Jerry Jones" imagePath="/img/jerry_j.png" role="Treasurer/GVSC Team Lead" email="jerryjon@mtu.edu" active=""/>
                         <ManagerCard name="Dr. Robert Pastel" imagePath="/img/robert_p.jpg" role="Faculty Advisor" email="pastel@mtu.edu" active=""/>
-                        <ManagerCard name="Jeremiah Kollodge" imagePath="/img/no_profile.png" role="CCLC Queue Team Lead" email="jtkollod@mtu.edu" active=""/>
+                        <ManagerCard name="Jeremiah Kollodge" imagePath="/img/jeremiah_k.jpg" role="CCLC Queue Team Lead" email="jtkollod@mtu.edu" active=""/>
                         <ManagerCard name="Zane Smalley" imagePath="/img/zane_s.png" role="Chem Store Team Lead" email="zasmalle@mtu.edu" active=""/>
                         <ManagerCard name="Dee Paulson" imagePath="/img/dee_p.jpg" role="CS Dept Scheduling App Team Lead" email="dcpaulso@mtu.edu" active=""/>
-                        <ManagerCard name="Benjamin Wittrup" imagePath="/img/ben_w.png" role="GDLS Team Co-Lead" email="bowittru@mtu.edu" active=""/>
-                        <ManagerCard name="Patrick Janssen" imagePath="/img/patrick_j.jpg" role="GDLS Team Co-Lead" email="pdjansse@mtu.edu" active=""/>
+                        <ManagerCard name="Patrick Janssen" imagePath="/img/patrick_j.jpg" role="GDLS Team Lead" email="pdjansse@mtu.edu" active=""/>
                         <ManagerCard name="Olivia O'Dwyer" imagePath="/img/olivia_o.jpg" role="Human Factors Team Lead" email="obodwyer@mtu.edu" active=""/>
-                        <ManagerCard name="Debbie Brink" imagePath="/img/debbie_b.jpg" role="Little Brothers Co-Team Lead" email="dfbrink@mtu.edu" active=""/>
-                        <ManagerCard name="Sid Regmi" imagePath="/img/sid_r.jpg" role="Little Brothers Co-Team Lead" email="ssregmi@mtu.edu" active=""/>
+                        <ManagerCard name="Debbie Brink" imagePath="/img/debbie_b.png" role="Little Brothers Co-Team Lead" email="dfbrink@mtu.edu" active=""/>
+                        <ManagerCard name="Sid Regmi" imagePath="/img/sid_r.png" role="Little Brothers Co-Team Lead" email="ssregmi@mtu.edu" active=""/>
                         <ManagerCard name="Logan Woznak" imagePath="/img/logan_w.jpg" role="SLS Team Lead" email="ljwoznak@mtu.edu" active=""/>
                         <ManagerCard name="Brandon St. Louis" imagePath="/img/brandon_l.png" role="Traverse City Electric Co-Team Lead" email="brstloui@mtu.edu" active=""/>
                         <ManagerCard name="Lela Root" imagePath="/img/lela_r.jpg" role="Traverse City Electric Co-Team Lead" email="lnroot@mtu.edu" active=""/>
